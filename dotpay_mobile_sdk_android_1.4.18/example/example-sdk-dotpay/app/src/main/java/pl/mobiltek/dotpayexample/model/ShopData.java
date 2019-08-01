f27b9f801bca8c2c76f82b00cdbe63da82b00fd3
wpawel <pawel.wojcik@gmail.com>
2019-08-01 14:09:55 +0200

    Update ShopData.java

--- dotpay_mobile_sdk_android_1.4.18/example/example-sdk-dotpay/app/src/main/java/pl/mobiltek/dotpayexample/model/ShopData.java
+++ dotpay_mobile_sdk_android_1.4.18/example/example-sdk-dotpay/app/src/main/java/pl/mobiltek/dotpayexample/model/ShopData.java
@@ -6,17 +6,17 @@ public class ShopData {
 
     private static final String name = "Jan";
     private static final String lastName = "Janowy";
-    private static final String email = "j.janowy@jan.pl";
+    private static final String email = "test@example.com";
 
     //    private  final String merchantId = "777777";
     private static final String merchantId = "855021";
     private static final double productPrice = 1759;
 
-    private static final String description = "Za  tel. Sony Xperia Z3";
+    private static final String description = "Test payment for Sony Xperia Z3";
     private static final String currency = "PLN";
     private static final String language = "pl";
 
-    private static final PaymentCardData paymentCard = new PaymentCardData("4444 4444 4444 4448", "448", "Jan", "Nowak", "08", "2017");
+    private static final PaymentCardData paymentCard = new PaymentCardData("4444 4444 4444 4448", "448", "Jan", "Nowak", "08", "2020");
 
 
     public static String getName() {
